d78846c85dc7d138234e9cda6ca9106f39490420
keenet <keenet@X-KNOY306PC06>
2024-01-31 13:14:03 -0500

    lab4

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -3,9 +3,67 @@
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+void printMenu() 
 {
-    std::cout << "Hello World!\n";
+	cout << "Please Select which operation to perform:" << endl;
+	cout << "\t1. Factorial" << endl;
+	cout << "\t2. Arithmetic Series" << endl;
+	cout << "\t3. Geometric Series" << endl;
+	cout << "\t4. Exit" << endl;
+	cout << "Your Selection: ";
+}
+void factorial() 
+{
+	
+}
+
+void arithmetic() 
+{
+	
+}
+void geometric() 
+{
+	
+}
+int main() {
+	int choice;
+	char again;
+	do {
+		printMenu();
+		cin >> choice;
+		// Quit if user chooses to exit (or any invalid choice)
+		if (choice > 3 || choice < 1) {
+			return 0;
+		}
+		else if (choice == 1) 
+		{
+			factorial();
+
+			cout << "Factorial: " << endl;
+			int N;
+
+			cout << "Enter a number: ";
+			cin >> N;
+
+			if (N < 1)
+			{
+				cout << "Nice try, please enter a POSTIVE number...: ";
+				cin >> N;
+			}
+		}
+
+		else if (choice == 2) {
+			arithmetic();
+		}
+
+		else if (choice == 3) {
+			geometric();
+		}
+		cout << "Go Again? [Y/N] ";
+		cin >> again;
+	} while (again == 'y' || again == 'Y');
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
